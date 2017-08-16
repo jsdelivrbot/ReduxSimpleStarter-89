f3a1f9d495802d6fee5f364a7891fbadef89f1d6
Sanjiv Jha <narutosanjiv@gmail.com>
2017-08-17 01:44:40 +0530

    Added Video Search and click functionality, used callback and state in ReactJS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -19,21 +20,29 @@ class App extends Component{
             videos: [],
             selectedVideo: null
         }
-        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-           this.setState({
-               videos: videos,
-               selectedVideo: videos[0]
-           });
+        this.videoSearch('surfboards');
+    }
+
+    videoSearch(term){
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
 
         });
     }
 
     render(){
+
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
         return (
+
             <div>
-                <SearchBar/>
+                <SearchBar onSearchTermChange={videoSearch}/>
                 <VideoDetail video={this.state.selectedVideo}/>
-                <VideoList videos={this.state.videos}/>
+                <VideoList onVideoSelect={selectedVideo => this.setState({ selectedVideo })} videos={this.state.videos}/>
             </div>
         )
     }
